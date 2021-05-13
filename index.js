322a36cfa03a1bb65e03bd715d1b8eb41f9e6c19
tallglassofwalter <73105310+tallglassofwalter@users.noreply.github.com>
2021-05-13 14:16:51 -0700

    add update function

--- index.js
+++ index.js
@@ -4,36 +4,102 @@ const todoInput = document.querySelector('.todo__input');
 const todoList = document.querySelector('.todo__list');
 
 // event listeners
+window.addEventListener('load', renderTodo);
 todoForm.addEventListener("submit", addTodo);
 
+let todoArr = ["pay bills", "buy groceries"];
+
 // functions
-function createTodo(str) {
-  let todoContainer = document.createElement('div');
-  let newTodo = document.createElement('li');
-  newTodo.innerText = str;
-  todoContainer.appendChild(newTodo);
-  newTodo.addEventListener('click', toggleTodo);
+function renderTodo() {
+  todoList.innerHTML = '';
 
-  let deleteBtn = document.createElement('button');
-  deleteBtn.innerHTML = 'Delete';
-  deleteBtn.classList.add('todo__delete');
-  deleteBtn.addEventListener('click', deleteTodo);
+  for (let i = 0; i < todoArr.length; i++) {
+    let newTodo = document.createElement('li');
+    let todoDiv = document.createElement('div');
+    todoDiv.setAttribute('id', i);
+    todoDiv.innerText = todoArr[i];
+    newTodo.appendChild(todoDiv);
+    todoDiv.addEventListener('click', toggleTodo);
 
-  todoContainer.appendChild(deleteBtn);
-  return todoContainer;
+    let editBtn = document.createElement('button');
+    editBtn.innerHTML = 'Edit';
+    editBtn.classList.add('todo__edit');
+    editBtn.setAttribute('type', 'button');
+    editBtn.addEventListener('click', editTodo);
+    newTodo.appendChild(editBtn);
+
+    let deleteBtn = document.createElement('button');
+    deleteBtn.innerHTML = 'Delete';
+    deleteBtn.classList.add('todo__delete');
+    deleteBtn.addEventListener('click', deleteTodo);
+
+    newTodo.appendChild(deleteBtn);
+    let editForm = document.createElement('form');
+    editForm.classList.add('hidden');
+    editForm.addEventListener('submit', saveTodo);
+
+    let errorDiv = document.createElement('div');
+    errorDiv.innerText = '';
+    editForm.appendChild(errorDiv);
+
+    let editInput = document.createElement('input');
+    editInput.setAttribute('type', 'text');
+    editInput.setAttribute('value', todoArr[i]);
+    editForm.appendChild(editInput);
+
+    let saveBtn = document.createElement('button');
+    saveBtn.innerHTML = 'Save';
+    editForm.appendChild(saveBtn);
+
+    let cancelBtn = document.createElement('button');
+    cancelBtn.setAttribute('type', 'button');
+    cancelBtn.innerHTML = 'Cancel';
+    cancelBtn.addEventListener('click', cancelTodo);
+    editForm.appendChild(cancelBtn);
+
+    newTodo.appendChild(editForm);
+
+    todoList.appendChild(newTodo);
+  }
+}
+
+function cancelTodo(e) {
+  let el = e.target.parentElement.parentElement.children;
+  [...el].forEach((el) => el.classList.toggle('hidden'));
+
 }
 
 function addTodo(e) {
     e.preventDefault();
     if (!todoInput.value) return null;
-    let todoContainer = createTodo(todoInput.value);
-    todoList.appendChild(todoContainer);
-    todoInput.value = "";
+    todoArr.push(todoInput.value);
+    renderTodo();
+    todoInput.value = '';
 }
 
 function deleteTodo(e) {
-  let todo = e.target.parentElement;
-  todo.remove();
+  let idx = e.target.previousSibling.id;
+  todoArr.splice(idx, 1);
+  renderTodo();
+}
+
+function editTodo(e) {
+  console.log('efit', e.target.parentElement)
+  let el = e.target.parentElement.children;
+  [...el].forEach((el) => el.classList.toggle('hidden'));
+}
+
+function saveTodo(e) {
+  e.preventDefault();
+  let val = e.target[0].value;
+  if (!val) {
+    let div = e.target.parentElement.lastChild.firstChild;
+    div.innerText = 'Input cannot be blank.';
+  } else {
+    let idx = e.target.parentElement.firstChild.id;
+    todoArr[idx] = val;
+    renderTodo();
+  }
 }
 
 function toggleTodo(e) {
